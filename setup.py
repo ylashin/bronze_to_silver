ecbdf099075bdbc73c30d84d9f3853a384da95b6
Yousry Mohamed <yousry.mohamed@mantelgroup.com.au>
2025-04-27 09:59:25 +1000

    Initial project setup with Databricks integration, including configuration files, README, and example code for data processing and testing.

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""
+setup.py configuration script describing how to build and package this project.
+
+This file is primarily used by the setuptools library and typically should not
+be executed directly. See README.md for how to deploy, test, and run
+the bronze_to_silver project.
+"""
+
+from setuptools import setup, find_packages
+
+import sys
+
+sys.path.append("./src")
+
+import datetime
+import bronze_to_silver
+
+local_version = datetime.datetime.utcnow().strftime("%Y%m%d.%H%M%S")
+
+setup(
+    name="bronze_to_silver",
+    # We use timestamp as Local version identifier (https://peps.python.org/pep-0440/#local-version-identifiers.)
+    # to ensure that changes to wheel package are picked up when used on all-purpose clusters
+    version=bronze_to_silver.__version__ + "+" + local_version,
+    url="https://databricks.com",
+    author="yousry.mohamed@mantelgroup.com.au",
+    description="wheel file based on bronze_to_silver/src",
+    packages=find_packages(where="./src"),
+    package_dir={"": "src"},
+    entry_points={
+        "packages": [
+            "main=bronze_to_silver.main:main",
+        ],
+    },
+    install_requires=[
+        # Dependencies in case the output wheel file is used as a library dependency.
+        # For defining dependencies, when this package is used in Databricks, see:
+        # https://docs.databricks.com/dev-tools/bundles/library-dependencies.html
+        "setuptools"
+    ],
+)
